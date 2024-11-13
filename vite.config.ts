4836a2a9a0ff5945682bf53fad712dde10074347
Tiaohsun <Tiaohsun@hotmail.com>
2024-11-13 18:02:24 +0800

    feat:增加type

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import dts from 'vite-plugin-dts'
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
         vueDevTools(),
+        dts({ include: ['src'] })
     ],
     build: {
         lib: {
